ddead60c67b3e7898401bf5a5a0a8637ada5be02
p0tt3r-iOS <france998811@gmail.com>
2021-03-17 23:47:41 +0900

    Complete DatePicker Review

---
+++ DatePicker_Review/DatePicker_Review/ViewController.swift
@@ -0,0 +1,38 @@
+//
+//  ViewController.swift
+//  DatePicker_Review
+//
+//  Created by 하동훈 on 2021/03/17.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var datePicker: UIDatePicker!
+    @IBOutlet weak var dateLabel: UILabel!
+    let dateFormatter: DateFormatter = {
+        let formatter = DateFormatter()
+        formatter.dateFormat = "eee MMM d h:mm a"
+        return formatter
+    }()
+    
+    @IBAction func didDatePickerChanged(_ sender: UIDatePicker) {
+        print("Value changed")
+        
+        let date: Date = sender.date
+        let dateString = dateFormatter.string(from: date)
+        
+        self.dateLabel.text = dateString
+    }
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.datePicker.addTarget(self, action: #selector(self.didDatePickerChanged(_:)), for: UIControl.Event.valueChanged)
+    }
+
+
+}
+
